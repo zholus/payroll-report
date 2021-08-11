40bb059d1f3d0e640be4ba0391b756ae4cf8047c
Alex <alzholus@gmail.com>
2021-08-11 22:42:17 +0200

    remove symfony/twig-bundle twig/extra-bundle twig/twig symfony/web-link symfony/validator symfony/string symfony/notifier symfony/mime symfony/http-client symfony /form symfony/intl

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
